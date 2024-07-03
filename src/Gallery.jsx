19d12ef272b4125aa16a92886dcfddd9cd65ff4d
Olixfills <oniabrahamfills@gmail.com>
2024-07-03 12:16:57 +0100

    ui fix

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-// import { storage } from './firebase';
-// import { ref, listAll, getDownloadURL } from 'firebase/storage';
+import axios from "axios";
 
 const Gallery = () => {
   const [images, setImages] = useState([]);
@@ -14,30 +13,24 @@ const Gallery = () => {
 
     const fetchImages = async () => {
       try {
-        const response = await fetch(
-          `https://api.cloudflare.com/client/v4/accounts/${accID}/images/v1`,
+        const response = await axios.get(
+          "https://api.cloudflare.com/client/v4/accounts/bbbbde9d437b7d633c9b7d9806510453/images/v1",
           {
-            method: "GET", // or 'POST' if needed
             headers: {
-              "Content-Type": "application/json",
-              // Add other headers as needed
-              Authorization: `Bearer ${token}`, // Example header
-              // 'Accept': 'application/json',
-              // 'Other-Custom-Header': 'HeaderValue'
+              Authorization: "Bearer 46xFCBZZvxwJJYKbmN53dY5FW3GndLzzzORjWs3k",
             },
           }
         );
-        const data = await response.json();
-        // Assuming the API returns an array of objects with `imageUrl`
+        console.log(response.data.result.images);
         setImages(
-          data.map((item) => ({
+          response?.data?.result?.images?.map((item) => ({
             ...item,
             original: item.imageUrl,
             thumbnail: item.thumbnailUrl,
           }))
         );
       } catch (error) {
-        console.error("Failed to fetch images:", error);
+        console.error("Error fetching images:", error);
       }
     };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,40 +1,56 @@
+// Import necessary components and styles
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import mainBg from "./assets/mainbg.jpeg";
 
-/* eslint-disable react/no-unescaped-entities */
 export default function HomePage() {
+  const [, setFile] = useState(null);
+
   return (
     <div
       className="bg-cover bg-center h-screen"
       style={{
-        backgroundImage:
-          "url('https://www.essence.com/wp-content/uploads/2023/06/fav-141-scaled.jpg')",
+        backgroundImage: `url(${mainBg})`,
         backgroundSize: "cover",
+        // height: "100vh",
+        // width: "100vw",
       }}
     >
       <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-50 text-white text-center">
         <h1
-          className="text-lg-7xl font-bold mb-4 capitalize text-orange-600"
+          className="text-5xl lg:text-7xl font-extrabold mb-4 capitalize text-orange-600"
           style={{
-            fontSize: { base: "lg", md: "7rem" },
             fontFamily: "cursive",
           }}
         >
           TheDeesLoveStory
         </h1>
-        <p className="text-xl mb-6">
+        <p className="text-md lg:text-xl mb-6 mx-3">
           Welcome to our wedding website! Join us in celebrating our special
           day. yaaaaaay
         </p>
         <div className="flex space-x-4 mt-28">
+          {/* Updated Link component */}
           <Link
             to="/gallery"
-            className="bg-white text-black px-6 py-3 rounded-md"
+            className="bg-white text-black px-6 py-3 rounded-md font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
           >
             View Gallery
           </Link>
-          <Link to="/form" className="bg-white text-black px-6 py-3 rounded-md">
-            Send Wishes
-          </Link>
+
+          {/* Updated Label and Input combination */}
+          <div className="bg-white text-black px-6 py-3 rounded-md font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+            <label htmlFor="file-upload" className="cursor-pointer ">
+              <span>Share Memory</span>
+              <input
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                onChange={(e) => setFile(e.target.files[0])}
+                className="sr-only"
+              />
+            </label>
+          </div>
         </div>
       </div>
     </div>
